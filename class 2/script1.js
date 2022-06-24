3703b2828148711a0983c3e478c37292001634f2
Jothikajothi <devloperjothikajothi@gmail.com>
2022-06-24 10:15:22 +0600

    secend

---
+++ class 2/script1.js
@@ -0,0 +1,14 @@
+// var x=10;
+// if(x==9){
+//     alert("the number is  right");
+// }else{
+//     alert("the number is wrong");
+// }
+//  var y=34;
+// if (y>50){
+//     console.log("the number is possitive");
+// }else if  ( Y <=30){
+//     console.log("the number is right"); 
+// }else{
+//     console.log("the number is not 30 and not 50");
+// }
